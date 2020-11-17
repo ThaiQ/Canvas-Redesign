5f9f3553fac39a9c7bfc45ee464edadd7072c0e3
ThaiQ <quachhongthai200@gmail.com>
2020-11-16 18:51:00 -0800

    Updates

---
+++ precache-manifest.e080784c9435d9daa9bf28636adffd4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f2934d8c617e35a9c0584bf590d2637",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15e8f0c9be4bdcf0679e",
+    "url": "/static/css/main.0a115f2d.chunk.css"
+  },
+  {
+    "revision": "07224eb7b38ea5c388f0",
+    "url": "/static/js/2.6bf07744.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6bf07744.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15e8f0c9be4bdcf0679e",
+    "url": "/static/js/main.3aa924a2.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
