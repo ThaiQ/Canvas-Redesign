e39911fd0d8c7471e239bc9d6be2a467f5db044f
HannyDuong <62577418+HannyDuong@users.noreply.github.com>
2020-12-08 14:15:11 -0800

    Dec8

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -10,18 +10,10 @@ import AssignmentsHub from './assignments/AssignmentsHub'
 import CreateAssignment from './assignments/CreateAssignment'
 import ViewAssignments from './assignments/ViewAssignments';
 
-<<<<<<< HEAD
-=======
 import Dashboard from './dashboard/dashboard'
->>>>>>> 8dce802537e65babcb32460885d5e8ee035dd3da
-import StarCourse from './course/StartCourse';
-import CourseHome from './course/CourseHome';
-import People from './course/People';
-import CourseExample from './course/example'
-<<<<<<< HEAD
-import Peo from './course/Peo';
-=======
->>>>>>> 8dce802537e65babcb32460885d5e8ee035dd3da
+import HomeCourse from './dashboard/course/courseHome';
+import Peo from './dashboard/course/Peo';
+import CourseExample from './courses/example'
 
 // Add new routing path to new pages in here
 let routes = [
@@ -42,35 +34,24 @@ let routes = [
     component: ViewAssignments
   },
   {
-<<<<<<< HEAD
-=======
     path: "/dashboard",
     component: Dashboard
   },
+ 
   {
->>>>>>> 8dce802537e65babcb32460885d5e8ee035dd3da
-    path: "/course",
-    component: StarCourse
-  },
-  {
-    path: "/course/CourseHome",
-    component: CourseHome
-  },
-  {
-    path: "/course/People",
-    component: People
-  },
-  {
-    path: "/course/example",
+    path: "/courses/example",
     component: CourseExample
-<<<<<<< HEAD
+  },
+
+  {
+    path: "/course/courseHome",
+    component: HomeCourse
   },
   {
     path: "/course/Peo",
     component: Peo
-=======
->>>>>>> 8dce802537e65babcb32460885d5e8ee035dd3da
-  }
+  },
+ 
 ]
 
 const routing = (
